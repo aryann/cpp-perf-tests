1e9c370cebea80ea13ec2c0f8b4b5c5b05810e1e
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-12-19 21:12:37 -0800

    Adds a benchmark that compares initializing a vector.

---
+++ benchmark/mutual_exclusion.cc
@@ -0,0 +1,53 @@
+// This is a benchmark that tests the latency of various methods for achieving
+// mutual exclusion on shared resources.
+//
+// The benchmarks all attempt to set values inside of a vector. All of the
+// benchmarks are single-threaded, however, we use the mutual exclusion
+// technique under test for each update. This allows us to do comparative
+// analysis between the various techniques.
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+#include <benchmark/benchmark.h>
+
+constexpr auto kInitValue = 100;
+
+static void BM_InitVectorWithStdMutex(benchmark::State& state) {
+    std::mutex mutex;
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        std::vector<std::int64_t> items(state.range(0), 0);
+        state.ResumeTiming();
+
+        for (int i = 0; i < items.size(); ++i) {
+            const std::lock_guard<std::mutex> lock(mutex);
+            items[i] = kInitValue;
+        }
+    }
+}
+
+static void BM_InitVectorWithCompareAndSet(benchmark::State& state) {
+    for (auto _ : state) {
+        state.PauseTiming();
+        std::vector<std::atomic<std::int64_t>> items(state.range(0));
+        state.ResumeTiming();
+
+        for (int i = 0; i < items.size(); ++i) {
+            std::int64_t expected = 0;
+            std::int64_t desired = kInitValue;
+            if (!items[i].compare_exchange_strong(expected, desired)) {
+                // This should never happen!
+                std::abort();
+            }
+        }
+    }
+}
+
+BENCHMARK(BM_InitVectorWithStdMutex)
+    ->Range(2 << 10, 2 << 20);
+BENCHMARK(BM_InitVectorWithCompareAndSet)
+    ->Range(2 << 10, 2 << 20);
+
+BENCHMARK_MAIN();
